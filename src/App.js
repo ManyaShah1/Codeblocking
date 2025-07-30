8732ebd1bc438a66ca1819ad61797d547f2a2742
ManyaShah1 <manya1105s@gmail.com>
2025-07-31 03:43:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BlocklyWorkspace from './BlocklyWorkspace';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CodeBlocking 🧩</h1>
+      <BlocklyWorkspace />
     </div>
   );
 }
